6c02bfd6f2f9cdcc926f2f7dc1a6ee97f5c10a1f
hedon954 <171725713@qq.com>
2022-09-07 21:24:03 +0800

    add: bulk reply

--- resp/reply/reply.go
+++ resp/reply/reply.go
@@ -1,5 +1,33 @@
 package reply
 
+import (
+	"fmt"
+)
+
+var (
+	nullBulkReplyBytes = []byte("$-1")
+	CRLF               = "\r\n"
+)
+
+// BulkReply represents a msg redis replies to client
+type BulkReply struct {
+	Arg []byte
+}
+
+func (b *BulkReply) ToBytes() []byte {
+	if len(b.Arg) == 0 {
+		return nullBulkReplyBytes
+	}
+	// hedon -> $5\r\nhedon\r\n
+	return []byte(fmt.Sprintf("$%d%s%s%s", len(b.Arg), CRLF, b.Arg, CRLF))
+}
+
+func MakeBulkReply(arg []byte) *BulkReply {
+	return &BulkReply{
+		Arg: arg,
+	}
+}
+
 // ErrorReply defines error reply
 type ErrorReply interface {
 	Error() string
