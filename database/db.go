ca8d47c486b32eca1736c7b2c72d39fa33d28ddb
hedon954 <171725713@qq.com>
2022-09-10 13:24:40 +0800

    add: DB.exec

--- database/db.go
+++ database/db.go
@@ -3,6 +3,8 @@ package database
 import (
 	"go-redis/datastruct/dict"
 	"go-redis/interface/resp"
+	"go-redis/resp/reply"
+	"strings"
 )
 
 // DB represent a redis database
@@ -24,3 +26,25 @@ type ExecFun func(db *DB, args [][]byte) resp.Reply
 
 // CmdLine redis command
 type CmdLine [][]byte
+
+// Exec runs redis command
+func (db *DB) Exec(c resp.Connection, cmdLine CmdLine) resp.Reply {
+	if len(cmdTable) <= 0 {
+		return reply.MakeStandardErrReply("ERR empty cmd")
+	}
+	cmdName := strings.ToLower(string(cmdLine[0])) // PING, SET, GET, etc..
+	cmd, ok := cmdTable[cmdName]
+	if !ok {
+		return reply.MakeStandardErrReply("ERR unknown command " + cmdName)
+	}
+
+	if !validateArity(cmd.arity, cmdLine) {
+		return reply.MakeArgNumErrReply(cmdName) // SET key
+	}
+	return cmd.executor(db, cmdLine[1:]) // SET k v -> k v
+}
+
+// validateArity checks arity validation
+func validateArity(arity int, cmdArgs [][]byte) bool {
+	return true
+}
