0c31910b7cd92d3d40a334b59864af16a73980ba
hedon954 <171725713@qq.com>
2022-09-12 13:32:36 +0800

    add: client pool

--- cluster/client_pool.go
+++ cluster/client_pool.go
@@ -2,36 +2,49 @@ package cluster
 
 import (
 	"context"
+	"fmt"
+
+	"go-redis/resp/client"
+
 	pool "github.com/jolestar/go-commons-pool/v2"
 )
 
 // connectionFactory provides the operations to control connection pool
 // implements pool.PooledObjectFactory interface
 type connectionFactory struct {
+
+	// node address
 	Peer string
 }
 
-func (c connectionFactory) MakeObject(ctx context.Context) (*pool.PooledObject, error) {
-	//TODO implement me
-	panic("implement me")
+func (cf connectionFactory) MakeObject(ctx context.Context) (*pool.PooledObject, error) {
+	redisClient, err := client.MakeClient(cf.Peer)
+	if err != nil {
+		return nil, err
+	}
+
+	redisClient.Start()
+	return pool.NewPooledObject(redisClient), nil
 }
 
-func (c connectionFactory) DestroyObject(ctx context.Context, object *pool.PooledObject) error {
-	//TODO implement me
-	panic("implement me")
+func (cf connectionFactory) DestroyObject(ctx context.Context, object *pool.PooledObject) error {
+	c, ok := object.Object.(*client.Client)
+	if !ok {
+		return fmt.Errorf("type mismatch")
+	}
+
+	c.Close()
+	return nil
 }
 
-func (c connectionFactory) ValidateObject(ctx context.Context, object *pool.PooledObject) bool {
-	//TODO implement me
-	panic("implement me")
+func (cf connectionFactory) ValidateObject(ctx context.Context, object *pool.PooledObject) bool {
+	return true
 }
 
-func (c connectionFactory) ActivateObject(ctx context.Context, object *pool.PooledObject) error {
-	//TODO implement me
-	panic("implement me")
+func (cf connectionFactory) ActivateObject(ctx context.Context, object *pool.PooledObject) error {
+	return nil
 }
 
-func (c connectionFactory) PassivateObject(ctx context.Context, object *pool.PooledObject) error {
-	//TODO implement me
-	panic("implement me")
+func (cf connectionFactory) PassivateObject(ctx context.Context, object *pool.PooledObject) error {
+	return nil
 }
